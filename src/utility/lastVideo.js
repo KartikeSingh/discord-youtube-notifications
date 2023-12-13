0b8759b80fc753f278b52f583b8355618b753bd9
ShadoW <89727583+shadowcool@users.noreply.github.com>
2023-12-13 14:52:16 +0530

    Update lastVideo.js

--- src/utility/lastVideo.js
+++ src/utility/lastVideo.js
@@ -1,35 +1,37 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const parser = new (require('rss-parser'))();
 
 module.exports = function (youtubeChannel) {
-    return new Promise(res => {
+    return new Promise(async res => {
         if (this.apiKey) {
-            fetch(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${youtubeChannel.youtube}&key=${this.apiKey}`).then(v => {
+            const playlistData = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${youtubeChannel.youtube}&key=${this.apiKey}`)
 
-                if(!v.data.items[0]) res(false);
+            if(![200, 201].includes(playlistData.status)) return res({})
+            
+            const v = await playlistData.json();
 
-                let playlistId = v.data.items[0].contentDetails.relatedPlaylists.uploads;
+            if(v.pageInfo.totalResults < 1) return res({});
 
-                fetch(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${youtubeChannel.youtube}&key=${this.apiKey}`).then(e => {
-                        
-                        if(!e.data.items[0]) res({});
-    
-                        let data = {
-                            title: e.data.items[0].snippet.title,
-                            author: e.data.items[0].snippet.channelTitle,
-                            thumbnail: (e.data.items[0].snippet.thumbnails.high || e.data.items[0].snippet.thumbnails.default).url,
-                            link: `https://www.youtube.com/watch?v=${e.data.items[0].snippet.resourceId.videoId}`,
-                            pubDate: e.data.items[0].snippet.publishedAt,
-                        }
+            let playlistId = v.items[0].contentDetails.relatedPlaylists.uploads;
 
-                        res(data);
-                }).catch(() => {
-                    res(false)
-                })
-            }).catch(() => {
-                res(false)
-            })
+            const data = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=1&key=${this.apiKey}`)
+
+            if(![200, 201].includes(data.status)) return res({})
+
+            const e = await data.json();
+
+            if(e.pageInfo.totalResults < 1) return res({});
+
+            let ytData = {
+                title: e.items[0].snippet.title,
+                author: e.items[0].snippet.channelTitle,
+                thumbnail: (e.items[0].snippet.thumbnails.high || e.items[0].snippet.thumbnails.default).url,
+                link: `https://www.youtube.com/watch?v=${e.items[0].snippet.resourceId.videoId}`,
+                pubDate: e.items[0].snippet.publishedAt,
+            }
+
+            return res(ytData);
         } else {
             parser.parseURL("https://www.youtube.com/feeds/videos.xml?channel_id=" + youtubeChannel.youtube).then(v => {
                 res(v.items[0] || {});
